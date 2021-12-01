52625e58218c25d1f4f76fe7c42001a9d4588fc9
John Gellert <gellertjm@gmail.com>
2021-12-01 04:35:19 -0600

    Format user.router

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -1,33 +1,36 @@
-const express = require('express');
-const { rejectUnauthenticated } = require('../modules/authentication-middleware');
-const encryptLib = require('../modules/encryption');
-const pool = require('../modules/pool');
-const userStrategy = require('../strategies/user.strategy');
+const express = require("express");
+const {
+  rejectUnauthenticated,
+} = require("../modules/authentication-middleware");
+const encryptLib = require("../modules/encryption");
+const pool = require("../modules/pool");
+const userStrategy = require("../strategies/user.strategy");
 const router = express.Router();
 
-
-router.get('/', rejectUnauthenticated, (req, res) => {
+router.get("/", rejectUnauthenticated, (req, res) => {
   res.send(req.user);
 });
 
-
-router.post('/register', (req, res, next) => {  
+router.post("/register", (req, res, next) => {
   const username = req.body.username;
   const password = encryptLib.encryptPassword(req.body.password);
 
-  const queryText = 'INSERT INTO "user" (username, password) VALUES ($1, $2) RETURNING id';
-  pool.query(queryText, [username, password])
+  const queryText =
+    'INSERT INTO "user" (username, password) VALUES ($1, $2) RETURNING id';
+  pool
+    .query(queryText, [username, password])
     .then(() => res.sendStatus(201))
-    .catch(() => res.sendStatus(500));
+    .catch((error) => {
+      res.sendStatus(500);
+      console.log(error);
+    });
 });
 
-
-router.post('/login', userStrategy.authenticate('local'), (req, res) => {
+router.post("/login", userStrategy.authenticate("local"), (req, res) => {
   res.sendStatus(200);
 });
 
-
-router.post('/logout', (req, res) => {
+router.post("/logout", (req, res) => {
   req.logout();
   res.sendStatus(200);
 });
